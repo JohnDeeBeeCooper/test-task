9fd93f306f1fbc9de29b1f8f5a21f13ba66e7b27
JohnDeeBeeCooper <vladislav8519@gmail.com>
2019-02-04 21:35:53 +0300

    npm update and laid a foundation

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+//import axios from 'axios';
+import { Button } from 'react-bootstrap';
 
 export default class App extends Component {
 
     render() {
-        return (
-            <div>Test</div>
-        )
+        return (<Button>test</Button>);
+
     }
 }
